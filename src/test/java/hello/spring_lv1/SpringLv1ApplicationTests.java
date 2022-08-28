5d89e0b0055f3844529ee4216cb0add6683012c4
JaeHeon Lee <romeo92s@naver.com>
2022-08-28 21:19:46 +0900

    인프런 -스프링입문start

---
+++ src/test/java/hello/spring_lv1/SpringLv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.spring_lv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLv1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
